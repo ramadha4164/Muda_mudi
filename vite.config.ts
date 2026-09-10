69f56444d07f92b23677a270816f6909a91b8a5d
rahmat342 <rahmatrahmat342@gmail.com>
2026-09-10 21:05:55 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/0rg.muda_i/",
+
+  base: "/Muda_mudi/",
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
